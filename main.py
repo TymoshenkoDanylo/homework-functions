57ef6180e19dd7b2f1c142003681df8afbac4200
Tymoshenko Danylo <tymoshenko.danylo.a@gmail.com>
2024-01-07 20:42:30 +0200

    commit task-1

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
-# Завдання 1
-# Напишіть функцію, яка обчислює добуток елементів списку цілих.
-# Список передається як параметр. Отриманий результат повертається із функції.
-
-import random
-
-number_list = [random.randint(1, 5) for _ in range(5)]
-print(number_list)
-
-
-def product(new_list) -> int:
-    product_number = 1
-    for i in range(len(new_list)):
-        product_number *= new_list[i]
-
-    return product_number
-
-
-try:
-    product = product(number_list)
-except Exception as error:
-    print(f"ExceptionError: {error}")
-
-print(product)
+# # Завдання 1
+# # Напишіть функцію, яка обчислює добуток елементів списку цілих.
+# # Список передається як параметр. Отриманий результат повертається із функції.
+#
+# import random
+#
+# number_list = [random.randint(1, 5) for _ in range(5)]
+# print(number_list)
+#
+#
+# def product(new_list) -> int:
+#     product_number = 1
+#     for i in range(len(new_list)):
+#         product_number *= new_list[i]
+#
+#     return product_number
+#
+#
+# try:
+#     product = product(number_list)
+# except Exception as error:
+#     print(f"ExceptionError: {error}")
+#
+# print(product)
 
